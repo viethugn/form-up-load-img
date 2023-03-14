ffb9315d37126487c5b4a1a9fbe62323f40619e6
DESKTOP-TQRSS73\ASUS <viethung05000@gmail.com>
2023-03-14 07:37:19 +0700

    up v1

--- index.php
+++ index.php
@@ -2,25 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload image</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <!-- <link href="./app.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Upload image</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <!-- <link href="./app.css" rel="stylesheet">
     <link href="./tailwind/styles.css" rel="stylesheet"> -->
 </head>
 
 <body>
-    <div class="w-full h-100vh flex justify-center align-middle items-center mx-auto">
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label for="formFile" class="mb-2 inline-block text-neutral-700 dark:text-neutral-200">Chọn ảnh bạn muốn tải lên</label>
-            <input class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 dark:border-neutral-600 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 dark:text-neutral-200 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 dark:file:bg-neutral-700 file:px-3 file:py-[0.32rem] file:text-neutral-700 dark:file:text-neutral-100 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none" type="file" name="fileToUpload" id="fileToUpload">
-            <input class="bg-gray-400 mt-5 rounded-lg p-2" type="submit" value="Upload Image" name="submit">
-        </form>
-    </div>
-    <br>
-        <!-- <?php require './upload.php'; ?> -->
+  <div class="w-full h-100vh flex justify-center align-middle mx-auto">
+    <form action="" method="post" enctype="multipart/form-data">
+      <label for="formFile" class="mb-2 inline-block text-neutral-700 dark:text-neutral-200">Chọn ảnh bạn muốn tải lên</label>
+      <input class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 dark:border-neutral-600 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 dark:text-neutral-200 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 dark:file:bg-neutral-700 file:px-3 file:py-[0.32rem] file:text-neutral-700 dark:file:text-neutral-100 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none" type="file" name="fileToUpload" id="fileToUpload">
+      <input class="bg-gray-400 mt-5 rounded-lg p-2" type="submit" value="Upload Image" name="submit">
+    </form>
+  </div>
+  <div class="flex justify-center align-middle">
+    <?php require './upload.php'; ?>
+  </div>
 </body>
 
 </html>
--- upload.php
+++ upload.php
@@ -4,14 +4,13 @@ class UploadImage
   public $target_file;
   public $imageFileType;
   public $target_dir;
-  public $uploadOk;
 
-  function __construct($target_file, $imageFileType, $target_dir, $uploadOk)
+  function __construct($target_file, $imageFileType, $target_dir)
   {
     $this->target_file = $target_file;
     $this->imageFileType = $imageFileType;
     $this->target_dir = $target_dir;
-    $this->uploadOk = $uploadOk;
+   
   }
 
   function get_target_file()
@@ -26,51 +25,47 @@ class UploadImage
   {
     return $this->target_dir;
   }
-  function get_uploadOk()
-  {
-    return $this->uploadOk;
-  }
+  // function get_uploadOk()
+  // {
+  //   return $this->uploadOk;
+  // }
 
   // call all function
   public function init()
   {
-    if ($this->checkImage() === 0) {
-      $this->checkImage();
+    if ($this->checkNoInputImageWhenSubmit() === true) { //check input empty
+      echo "<br><br>Bạn chưa chọn tệp!";
+    } elseif ($this->checkFileExist() === true) {//check file exist
+      echo "<br><br>Thông báo, tệp này đã tồn tại!";
+    } elseif ($this->checkFileSize()) {
+      echo "<br><br>Thông báo, file bạn đưa vào lớn hơn 5MB!";
+    } elseif ($this->checkTypeImage() === true) {//check type is type img
+      echo "<br><br>Thông báo, chỉ cho phép các file có định dạng sau JPG, JPEG, PNG & GIF";
+    } elseif ($this->checkCountFileImgInDir() === true ){//check dir <=5
+      echo "<br><br>Số lượng tệp đã đầy";
     } else {
-      $this->checkFileExist();
-      $this->checkFileSize();
-      $this->checkTypeImage();
+      $this->checkInputImageWhenSubmit();
       $this->checkUploadOkToUpload();
-      $this->checkCountFileInDir();
     }
   }
 
-  private function checkImage()
+  public function checkNoInputImageWhenSubmit()
   {
-    // Check if image file is a actual image or fake image
-    if (isset($_POST["submit"])) {
-      if (empty($_FILES["fileToUpload"]["tmp_name"])) {
-        echo "<br><br>Bạn chưa chọn file";
-        return 0;
-      } else {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if ($check !== false) {
-          echo "<br><br>File ảnh là - " . $check["mime"] . ".";
-          $this->uploadOk = 1;
-        } else {
-          echo "<br><br>Tệp không phải là file hình!.";
-          $this->uploadOk = 0;
-        }
-      }
+    // Check if image file is empty
+    if (isset($_POST["submit"]) && empty($_FILES["fileToUpload"]["tmp_name"])) {
+      return true;
     }
+    return false;
   }
 
-  public function checkFileExist()
-  {
-    // Check if file already exists
-    if (file_exists($this->get_target_file())) {
-      echo "<br><br>Xin lỗi, file đã tồn tại!";
-      $this->uploadOk = 0;
+  public function checkInputImageWhenSubmit() {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+      echo "<br><br>File ảnh là - " . $check["mime"] . ".";
+      // $this->uploadOk = 1;
+    } else {
+      echo "<br><br>File không phải là file hình!.";
+      // $this->uploadOk = 0;
     }
   }
 
@@ -78,38 +73,43 @@ class UploadImage
   {
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 5120) {
-      echo "<br><br>Xin lỗi, file bạn đưa vào lớn hơn 5MB!";
-      $this->uploadOk = 0;
+      // $this->uploadOk = 0;
+      return true;
     }
+    return false;
   }
 
   public function checkTypeImage()
   {
     // Allow certain file formats
-    if (
-      $this->get_imageFileType() != "jpg" && $this->get_imageFileType() != "png" && $this->get_imageFileType() != "jpeg"
-      && $this->get_imageFileType() != "gif"
-    ) {
-      echo "<br><br>Thông báo, chỉ cho phép các file có định dạng sau JPG, JPEG, PNG & GIF";
-      $this->uploadOk = 0;
+    if ($this->get_imageFileType() != "jpg" && $this->get_imageFileType() != "png" && $this->get_imageFileType() != "jpeg" 
+      && $this->get_imageFileType() != "gif") {
+      // $this->uploadOk = 0;
+      return true;
     }
+    return false;
+  }
+
+  public function checkFileExist()
+  {
+    // Check if file already exists
+    if (file_exists($this->get_target_file())) {
+      // $this->uploadOk = 0;
+      return true;
+    }
+    return false;
   }
 
   public function checkUploadOkToUpload()
   {
-    // Check if $uploadOk is set to 0 by an error
-    if ($this->uploadOk == 0) {
-      echo "<br><br>Thông báo, file của bạn không đưa lên được!";
-      // if everything is ok, try to upload file
+    if ( move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $this->get_target_file())) {
+        echo "<br><br>Tệp " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " đã được tải lên!";
     } else {
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $this->get_target_file())) {
-        echo "<br><br>File " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " đã được tải lên!";
-      } else {
-        echo "<br><br><strong class='text-blue-300'>Xin lỗi, file ảnh khi tải lên có sự cố!.</strong>";
-      }
+        echo "<br><br><strong class='text-blue-300'>Thông báo, file ảnh khi tải lên có sự cố!.</strong>";
     }
   }
 
+  //Count image in dir
   public function countImage()
   {
     $total_files = 0;
@@ -126,17 +126,31 @@ class UploadImage
     return $total_files;
   }
 
-  public function checkCountFileInDir () {
-    if ($this->countImage() > 5) {
-      echo "<br><br>Số lượng tệp đã đầy";
+  public function checkCountFileImgInDir()
+  {
+    if ($this->countImage() >= 5) {
+      return true;
     }
+    return false;
   }
 }
 
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-$uploadimg = new UploadImage($target_file, $imageFileType, $target_dir, $uploadOk = '1');
-$uploadimg->init();
+//when event is submit
+if (isset($_POST["submit"])) {
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  // $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+  //Create new object
+  $uploadimg = new UploadImage($target_file, $imageFileType, $target_dir);
+  $uploadimg->init();
+  $temp = 0;
+
+}
+
+// $uploadimg = new UploadImage($target_file, $imageFileType, $target_dir, $uploadOk = '1');
+// $uploadimg->init();
+
+// echo "<br><br><<<a href='index.php'>Trở lại</a>>>";
